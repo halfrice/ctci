718b32b95b9b25fb0274d49e2373c93d6106fcdb
halfrice <neel@surrus.net>
2017-11-24 00:05:35 -0800

    working solution via ascii code values comparisons and basic test cases

--- src/c1/check_permutation.py
+++ src/c1/check_permutation.py
@@ -1,6 +1,11 @@
 # Check Permutation
 
-# Given two strings, wrote a method to decide if one is a permutation of the other.
+# Given two strings, write a method to decide if one is a permutation of the other.
 
 def check_permutation(s1, s2):
-  return
+  if len(s1) != len(s2):
+    return False
+  su = 0
+  for i in range(len(s1)):
+    su += ord(s1[i])-ord(s2[i])
+  return su == 0
--- tests/c1/test_check_permutation.py
+++ tests/c1/test_check_permutation.py
@@ -3,3 +3,4 @@ from src.c1.check_permutation import *
 def test_check_permutation():
   assert check_permutation('lmao', 'maol') == True
   assert check_permutation('wtfbro', 'nahman') == False
+  assert check_permutation('Lmao', 'maol') == False
