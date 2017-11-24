13f5eebd504a93a514231bb1255413703c96d6b6
halfrice <neel@surrus.net>
2017-11-23 21:41:27 -0800

    tests for 1.1 is_unique.py

--- tests/c1/test_is_unique.py
+++ tests/c1/test_is_unique.py
@@ -1,5 +1,9 @@
-from src.c1.is_unique import is_unique
+from src.c1.is_unique import *
 
 def test_is_unique():
-  test_string = 'lmaogg'
-  is_unique(test_string)
+  assert is_unique('lmao') == True
+  assert is_unique('roflgg') == False
+
+def test_is_unique_no_data_structures():
+  assert is_unique_no_data_structures('abcdefg') == True
+  assert is_unique_no_data_structures('abcdefgg') == False
