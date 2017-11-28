c2649a3d98227a2e602b4430010eb8875ee7f9b3
halfrice <neel@surrus.net>
2017-11-28 14:05:06 -0800

    unit testing for basic linked list functions and remove_dups

---
+++ tests/c2/test_remove_dups.py
@@ -0,0 +1,28 @@
+from src.c2.remove_dups import *
+
+ll = LinkedList()
+
+def test_linked_list():
+  assert isinstance(ll, LinkedList)
+  assert ll.is_empty()
+
+def test_add():
+  ll.add('a')
+  ll.add('b')
+  ll.add('c')
+  ll.add('c')
+  ll.add('x')
+  ll.add('x')
+  ll.add('d')
+  assert ll.stringify() == 'dxxccba'
+
+def test_remove():
+  ll.remove('x')
+  assert ll.stringify() == 'dccba'
+
+def test_remove_dups():
+  ll.add('c')
+  ll.add('c')
+  ll.stringify()
+  ll.remove_dups('c')
+  assert ll.stringify() == 'cdba'
