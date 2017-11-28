332c50475ae7f8c46a3ad18c283ca33a0638ef70
halfrice <neel@surrus.net>
2017-11-27 23:30:36 -0800

    and another one. -Biggie Smalls

--- tests/c1/test_string_rotation.py
+++ tests/c1/test_string_rotation.py
@@ -2,3 +2,4 @@ from src.c1.string_rotation import *
 
 def test_string_rotation():
   assert string_rotation('erbottlewat', 'waterbottle') is True
+  assert string_rotation('erbottlewut', 'waterbottle') is False
