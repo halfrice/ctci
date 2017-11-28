3201683a73de4f19a4cd62a7fadce3726a513cb5
halfrice <neel@surrus.net>
2017-11-27 23:29:22 -0800

    O(n) time complexity and O(n) space complexity solution

--- src/c1/string_rotation.py
+++ src/c1/string_rotation.py
@@ -5,4 +5,11 @@
 # isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
 
 def string_rotation(s1, s2):
-  return
+  if len(s1) != len(s2):
+    return False
+  for i in range(len(s1)):
+    if s1[i] == s2[0]:
+      new_s1 = s1[i:]+s1[0:i]
+      if new_s1 == s2:
+        return True
+  return False
