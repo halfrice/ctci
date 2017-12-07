68e4eb6abab0fb1e937ddd6dc8226f50f6f522ea
halfrice <neel@surrus.net>
2017-12-07 15:52:09 -0800

    solution with buffer

--- src/c2/remove_dups.py
+++ src/c2/remove_dups.py
@@ -3,64 +3,26 @@
 # Write code to remove duplicates from an unsorted linked list. How would you solve this problem
 # if a temporary buffer is not allowed?
 
-class Node:
-  def __init__(self, data=None):
-    self.data = data
-    self.next = None
+from src.c2.common.linked_list import LinkedList
 
-class LinkedList:
-  def __init__(self, head=None):
-    self.head = head
-
-  def is_empty(self):
-    return self.head == None
-
-  def add(self, data):
-    new_node = Node(data)
-    new_node.next = self.head
-    self.head = new_node
-
-  def remove(self, data):
-    # search and destroy all
-    node = self.head
-    prev = None
-    while node:
-      if node.data == data and prev:
-        prev.next = node.next
-      elif node.data == data and not prev:
-        self.head = node.next
-      prev = node
-      node = node.next
+class RemoveDups(LinkedList):
+  # def __init__(self):
+    # LinkedList.__init__(self)
 
   def remove_dups(self, data):
     node = self.head
     prev = None
-    keep_node = None
-    # find node to keep
-    while not keep_node:
-      if node.data == data:
-        keep_node = node
-      node = node.next
-    # find and forget any duplicate nodes until a non-dup is found
-    found_dup = False
+    cut = None
     while node:
       if node.data == data:
-        found_dup = True
-      elif node.data != data and found_dup:
-        prev.next = node
-        found_dup = False
+        if cut:
+          prev.next = node.next
+        else:
+          prev = node
+          cut = node
       else:
         prev = node
       node = node.next
 
   def remove_dups_no_buffer(self, data):
-    self.remove(data)
-    self.add(data)
-
-  def stringify(self):
-    node = self.head
-    s = ''
-    while node:
-      s += node.data
-      node = node.next
-    return s
+    return
--- tests/c2/test_remove_dups.py
+++ tests/c2/test_remove_dups.py
@@ -1,35 +1,27 @@
-from src.c2.remove_dups import *
+from src.c2.remove_dups import RemoveDups
 
-ll = LinkedList()
+ll = RemoveDups()
 
-def test_linked_list():
-  assert isinstance(ll, LinkedList)
-  assert ll.is_empty()
-
-def test_add():
+def test_remove_dups():
   ll.add('a')
+  ll.add('b')
   ll.add('b')
   ll.add('c')
   ll.add('c')
-  ll.add('x')
-  ll.add('x')
-  ll.add('d')
-  assert ll.stringify() == 'dxxccba'
-
-def test_remove():
-  ll.remove('x')
-  assert ll.stringify() == 'dccba'
-
-def test_remove_dups():
   ll.add('c')
-  ll.add('c')
-  ll.stringify()
+  ll.add('b')
+  ll.add('b')
+  ll.add('a')
   ll.remove_dups('c')
-  assert ll.stringify() == 'cdba'
+  assert ll.stringify() == 'abbcbba'
+  ll.remove_dups('b')
+  assert ll.stringify() == 'abca'
+  ll.remove_dups('a')
+  assert ll.stringify() == 'abc'
 
-def test_remove_dups_no_buffer():
-  ll.add('c')
-  ll.add('c')
-  ll.add('x')
-  ll.remove_dups_no_buffer('c')
-  assert ll.stringify() == 'dbaxc'
+# def test_remove_dups_no_buffer():
+#   ll.add('c')
+#   ll.add('c')
+#   ll.add('x')
+#   ll.remove_dups_no_buffer('c')
+#   assert ll.stringify() == 'xdba'
